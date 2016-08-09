fbe7112e23418328a186c68e78d1ef4683e3f69d
Mark Waller <M_P_waller@hotmail.com>
2016-08-09 16:22:54 +0800

    Update Main.java

--- src/main/java/org/wallerlab/dso/Main.java
+++ src/main/java/org/wallerlab/dso/Main.java
@@ -3,14 +3,10 @@ package org.wallerlab.dso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.wallerlab.config.BatchConfig;
 
 import java.io.IOException;
 
-
-
 @SpringBootApplication
-@Import(BatchConfig.class)
 public class Main {
 
     public static void main(String[] args) throws IOException {
